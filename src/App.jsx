630d4779c947fc2606f5bd9406fc4d7f142ebee5
Nomfundo Florence <nomfundomlangeni0@gmail.com>
2025-05-29 23:35:54 +0200

    responsive about me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HeroSection from './components/HeroSection'
+import About from './components/About'
 import './App.css'
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
     <>
       <Header />
       <HeroSection />
+      <About />
+      
       <Footer />
     </>
   )
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,15 +6,15 @@ export default function HeroSection() {
     <section className="hero-section">
       <img src={image} alt="image of Nomfundo smiling" />
       <h1 className="name">Nomfundo Mlangeni</h1>
-      <h3 className="professional-title">Software Developer</h3>
+      <h3 className="professional-title">Web Developer</h3>
       <div className="socials-container">
         <button className="github-button">
-          <a href="#github">
+          <a href="https://github.com/NomfundoFlorence" target="blank">
             GitHub
           </a>
         </button>
         <button className="linkedIn-button">
-          <a href="#linkedIn">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/nomfundo-mlangeni05/" target="blank">LinkedIn</a>
         </button>
       </div>
     </section>
