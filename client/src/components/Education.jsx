5d06b70e73eb88756654ca581e4e5e4f15a86c52
NomfundoFlorence <nomfundomlangeni0@gmail.com>
2025-08-16 01:07:59 +0200

    changed education input & formatted projects imgs

--- client/src/components/Education.jsx
+++ client/src/components/Education.jsx
@@ -1,39 +1,25 @@
 import "/public/Education.css";
-import nwu from "/images/nwu.png";
-import nhs from "/images/nigel_high.jpg";
+import education from "../assets/education.json";
 
 export default function Education() {
+  const educationList = education.map((edu, index) => {
+    return (
+      <div className="education-card" key={index}>
+        <div className="school-logo">
+          <img src={`/images/${edu.logo}`} alt={`${edu.schoolName}'s logo`} />
+        </div>
+        <h3 className="school-title">{edu.title}</h3>
+        <h3 className="school-name">{edu.schoolName}</h3>
+        <p className="dates">{edu.dates}</p>
+        <p className="school-description">{edu.description}</p>
+      </div>
+    );
+  });
+
   return (
     <section className="education-section" id="education">
       <h1>Education</h1>
-      <div className="education-content">
-        <div className="education-card">
-          <div className="school-logo">
-            <img src={nwu} alt="NWU logo" />
-          </div>
-          <h3 className="school-title">BSc in IT</h3>
-          <h3 className="school-name">North-West University</h3>
-          <p className="dates">May 2024</p>
-          <p className="school-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in
-            dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula. Sed
-            auctor neque eu tellus rhoncus ut eleifend nibh porttitor.
-          </p>
-        </div>
-        <div className="education-card">
-          <div className="school-logo">
-            <img src={nhs} alt="NHS logo" />
-          </div>
-          <h3 className="school-title">National Senior Certificate</h3>
-          <h3 className="school-name">Nigel High School</h3>
-          <p className="dates">2016 - 2020</p>
-          <p className="school-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in
-            dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula. Sed
-            auctor neque eu tellus rhoncus ut eleifend nibh porttitor.
-          </p>
-        </div>
-      </div>
+      <div className="education-content">{educationList}</div>
     </section>
   );
 }
